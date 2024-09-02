db8702525e602f2202062224b4096b8fb3c183d2
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-09-02 16:36:08 -0500

    creacion de la entidad Reserva

---
+++ src/main/java/com/unimag/travel/entities/Reserva.java
@@ -0,0 +1,29 @@
+package com.unimag.travel.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "reservas")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idReserva;
+
+    private Cliente cliente;
+
+    private Vuelo vuelo;
+    @Column(name = "fecha_de_reserva")
+    private LocalDate fechaDeReserva;
+    @Column(name = "numero_de_pasajeros")
+    private int numeroDePasajeros;
+}
