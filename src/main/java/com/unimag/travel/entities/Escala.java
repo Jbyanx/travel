1b4f6ed9189f902d5b28dc9eb8768f9dce515150
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-09-02 16:49:17 -0500

    creando entidad Escala

---
+++ src/main/java/com/unimag/travel/entities/Escala.java
@@ -0,0 +1,31 @@
+package com.unimag.travel.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CollectionIdJdbcTypeCode;
+
+import java.time.Duration;
+
+@Entity
+@Table(name = "escalas")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Escala {
+    @Id
+    private Long idEscala;
+
+    private Vuelo vuelo;
+
+    private Aeropuerto aeropuerto;
+    @Column(name = "tiempo_de_escala")
+    private Duration tiempoDeEscala;
+}
