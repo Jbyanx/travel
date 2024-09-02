c16dd99a02766d20cec42e01dbf6f8dcc2aa8636
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-09-02 16:35:59 -0500

    creacion de la entidad aerolinea

---
+++ src/main/java/com/unimag/travel/entities/Aerolinea.java
@@ -0,0 +1,22 @@
+package com.unimag.travel.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "aerolineas")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Aerolinea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAerolinea;
+    private String nombre;
+    private Long codigoDeAerolinea;
+    private String paisDeOrigen;
+}
