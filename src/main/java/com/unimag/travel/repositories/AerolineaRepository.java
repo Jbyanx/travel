cea10199211dc0a746d58cee073891afc2d4838a
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-09-07 23:34:25 -0500

    implementando el servicio de Aerolinea

--- src/main/java/com/unimag/travel/repositories/AerolineaRepository.java
+++ src/main/java/com/unimag/travel/repositories/AerolineaRepository.java
@@ -4,6 +4,9 @@ import com.unimag.travel.entities.Aerolinea;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AerolineaRepository extends JpaRepository<Aerolinea, Long> {
+    Optional<Aerolinea> getAerolineaByName(String name);
 }
---
+++ src/main/java/com/unimag/travel/services/impl/AerolineaServiceImpl.java
@@ -0,0 +1,60 @@
+package com.unimag.travel.services.impl;
+
+import com.unimag.travel.entities.Aerolinea;
+import com.unimag.travel.repositories.AerolineaRepository;
+import com.unimag.travel.services.AerolineaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AerolineaServiceImpl implements AerolineaService {
+    private AerolineaRepository aerolineaRepository;
+
+    @Autowired
+    public AerolineaServiceImpl(AerolineaRepository aerolineaRepository) {
+        this.aerolineaRepository = aerolineaRepository;
+    }
+
+
+    @Override
+    public Optional<Aerolinea> getAerolineaById(Long id) {
+        return aerolineaRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Aerolinea> getAerolineaByName(String name) {
+        return aerolineaRepository.getAerolineaByName(name);
+    }
+
+    @Override
+    public List<Aerolinea> getAllAerolineas() {
+        return aerolineaRepository.findAll();
+    }
+
+    @Override
+    public Aerolinea saveAerolinea(Aerolinea aerolinea) {
+        return aerolineaRepository.save(aerolinea);
+    }
+
+    @Override
+    public Aerolinea updateAerolineaById(Long id, Aerolinea saveAerolinea) {
+        Aerolinea aerolineaFromDb = aerolineaRepository.findById(id).get();
+        if(aerolineaFromDb != null) {
+            aerolineaFromDb.setNombre(saveAerolinea.getNombre());
+            aerolineaFromDb.setPaisDeOrigen(saveAerolinea.getPaisDeOrigen());
+            aerolineaFromDb.setVuelos(saveAerolinea.getVuelos());
+            aerolineaFromDb.setCodigoDeAerolinea(saveAerolinea.getCodigoDeAerolinea());
+        } else {
+            throw new RuntimeException("Aerolinea ID:" +id + " not found");
+        }
+        return aerolineaRepository.save(aerolineaFromDb);
+    }
+
+    @Override
+    public void deleteAerolineaById(Long id) {
+        aerolineaRepository.deleteById(id);
+    }
+}
