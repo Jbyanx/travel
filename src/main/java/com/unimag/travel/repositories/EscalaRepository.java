48d7bc32074efb3ec2f2d2d4fdd6ea345ea46d07
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-09-07 23:54:01 -0500

    implementando el servicio de Escala

--- src/main/java/com/unimag/travel/repositories/EscalaRepository.java
+++ src/main/java/com/unimag/travel/repositories/EscalaRepository.java
@@ -4,6 +4,9 @@ import com.unimag.travel.entities.Escala;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EscalaRepository extends JpaRepository<Escala, Long> {
+    Optional<Escala> getEscalaByName(String name);
 }
---
+++ src/main/java/com/unimag/travel/services/impl/EscalaServiceImpl.java
@@ -0,0 +1,59 @@
+package com.unimag.travel.services.impl;
+
+import com.unimag.travel.entities.Escala;
+import com.unimag.travel.repositories.EscalaRepository;
+import com.unimag.travel.services.EscalaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EscalaServiceImpl implements EscalaService {
+    private EscalaRepository escalaRepository;
+
+    @Autowired
+    public EscalaServiceImpl(EscalaRepository escalaRepository) {
+        this.escalaRepository = escalaRepository;
+    }
+
+
+    @Override
+    public Optional<Escala> getEscalaById(Long id) {
+        return escalaRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Escala> getEscalaByName(String name) {
+        return escalaRepository.getEscalaByName(name);
+    }
+
+    @Override
+    public List<Escala> getAllEscalas() {
+        return escalaRepository.findAll();
+    }
+
+    @Override
+    public Escala saveEscala(Escala escala) {
+        return escalaRepository.save(escala);
+    }
+
+    @Override
+    public Escala updateEscalaById(Long id, Escala saveEscala) {
+        Escala escalaFromDb = escalaRepository.findById(id).get();
+        if(escalaFromDb != null) {
+            escalaFromDb.setTiempoDeEscala(saveEscala.getTiempoDeEscala());
+            escalaFromDb.setAeropuerto(saveEscala.getAeropuerto());
+            escalaFromDb.setVuelo(saveEscala.getVuelo());
+        }else{
+            throw new RuntimeException("Escala not found");
+        }
+        return escalaRepository.save(escalaFromDb);
+    }
+
+    @Override
+    public void deleteEscalaById(Long id) {
+        escalaRepository.deleteById(id);
+    }
+}
