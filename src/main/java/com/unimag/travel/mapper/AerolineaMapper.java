cfdd728c6e99fd93bb05fa873aead7bd571e65fe
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-10-04 12:01:14 -0500

    especificando el uso del mapper de vuelo dentro del mapper de Aerolinea

--- src/main/java/com/unimag/travel/mapper/AerolineaMapper.java
+++ src/main/java/com/unimag/travel/mapper/AerolineaMapper.java
@@ -2,88 +2,41 @@ package com.unimag.travel.mapper;
 
 import com.unimag.travel.dto.response.GetAerolinea;
 import com.unimag.travel.dto.request.SaveAerolinea;
-import com.unimag.travel.dto.response.GetEscala;
-import com.unimag.travel.dto.response.GetVuelo;
 import com.unimag.travel.entities.Aerolinea;
-import com.unimag.travel.entities.Escala;
-import com.unimag.travel.entities.Vuelo;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper
+@Mapper(uses = {VueloMapper.class})
 public interface AerolineaMapper {
 
     AerolineaMapper INSTANCE = Mappers.getMapper(AerolineaMapper.class);
 
     //SAVE AEROLINEA
-    @Mappings({
-            @Mapping(target = "codigo", source = "codigoDeAerolinea"),
-            @Mapping(target = "pais", source = "paisDeOrigen")
-    })
+    @Mapping(target = "codigo", source = "codigoDeAerolinea")
+    @Mapping(target = "pais", source = "paisDeOrigen")
     SaveAerolinea aerolineaToSaveAerolinea(Aerolinea aerolinea);
 
-    @Mappings({
-            @Mapping(target = "codigoDeAerolinea" ,source = "codigo"),
-            @Mapping(target = "paisDeOrigen" , source = "pais"),
-    })
-    Aerolinea SaveAerolineaToAerolinea(SaveAerolinea saveAerolinea);
+    @Mapping(source = "codigo", target = "codigoDeAerolinea")
+    @Mapping(source = "pais", target = "paisDeOrigen")
+    Aerolinea saveAerolineaToAerolinea(SaveAerolinea saveAerolinea);
 
-    List<Aerolinea> saveAerolineaListToAerolineaList(List<SaveAerolinea> listSaveAerolinea);
-
-    List<SaveAerolinea> aerolineaListToSaveAerolineaList(List<Aerolinea> listAerolinea);
+    List<SaveAerolinea> aerolineaListToSaveAerolineaList(List<Aerolinea> aerolineaList);
+    List<Aerolinea> saveAerolineaListToAerolineaList(List<SaveAerolinea> saveAerolineaList);
 
     //GET AEROLINEA
-    @Mappings({
-            @Mapping(target ="codigo", source = "codigoDeAerolinea"),
-            @Mapping(target = "pais", source = "paisDeOrigen"),
-            @Mapping(target = "id", source = "idAerolinea")
-    })
+    @Mapping(target = "id", source = "idAerolinea")
+    @Mapping(target ="codigo", source = "codigoDeAerolinea")
+    @Mapping(target = "pais", source = "paisDeOrigen")
     GetAerolinea aerolineaToGetAerolinea(Aerolinea aerolinea);
 
-    @Mappings({
-            @Mapping(target ="codigoDeAerolinea", source = "codigo"),
-            @Mapping(target = "paisDeOrigen", source = "pais"),
-            @Mapping(target = "idAerolinea", source = "id")
-    })
+    @Mapping(source = "id", target = "idAerolinea")
+    @Mapping(source ="codigo", target = "codigoDeAerolinea")
+    @Mapping(source = "pais", target = "paisDeOrigen")
     Aerolinea GetAerolineaToAerolinea(GetAerolinea getAerolinea);
 
-    @Mappings({
-            @Mapping(target ="id", source = "idVuelo"),
-            @Mapping(target = "aeropuertoOrigen", source = "aeropuertoOrigen.nombre"),
-            @Mapping(target = "aeropuertoDestino", source = "aeropuertoDestino.nombre")
-    })
-    GetVuelo vueloToGetVuelo(Vuelo vuelo);
-
-    @Mappings({
-            @Mapping(target = "idVuelo", source = "id"),
-            @Mapping(target = "aeropuertoOrigen.nombre", source = "aeropuertoOrigen"),
-            @Mapping(target = "aeropuertoDestino.nombre", source = "aeropuertoDestino")
-    })
-    Vuelo getVueloToVuelo(GetVuelo getVuelo);
-
-    //listas de vuelos de la aerolinea
-    List<GetVuelo> vueloListToGetVueloList(List<Vuelo> vuelos);
-
-    List<Vuelo> getVueloListToVueloList(List<GetVuelo> getVuelos);
-
-    //listas de aerolineas
-    List<GetAerolinea> aerolineaListToGetAerolineaList(List<Aerolinea> aerolineas);
-
-    List<Aerolinea> getAerolineaListToAerolineaList(List<GetAerolinea> aerolineas);
-
-    //escalas dentro de GetVuelos
-    @Mapping(target = "aeropuerto", source = "aeropuerto.nombre")
-    GetEscala escalaToGetEscala(Escala escala);
-
-    @Mapping(target = "aeropuerto.nombre", source = "aeropuerto")
-    Escala getEscalaToEscala(GetEscala getEscala);
-
-    //listas de escalas
-    List<GetEscala> escalaListToGetEscalaList(List<Escala> escalas);
-
-    List<Escala> getEscalaListToEscalaList(List<GetEscala> getEscalaList);
+    List<GetAerolinea> aerolineaListToGetAerolineaList(List<Aerolinea> aerolineaList);
+    List<Aerolinea> getAerolineaListToAerolineaList(List<GetAerolinea> getAerolineaList);
 }
