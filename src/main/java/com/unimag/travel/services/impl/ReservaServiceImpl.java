5fac91957e52d6bb95be6c219da1dffb02ad03b0
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-10-05 02:05:00 -0500

    cambiando uso de entidades por dtos y haciendo los cambios necesarios

--- src/main/java/com/unimag/travel/services/impl/ReservaServiceImpl.java
+++ src/main/java/com/unimag/travel/services/impl/ReservaServiceImpl.java
@@ -1,11 +1,15 @@
 package com.unimag.travel.services.impl;
 
+import com.unimag.travel.dto.request.SaveReserva;
+import com.unimag.travel.dto.response.GetReserva;
 import com.unimag.travel.entities.Reserva;
+import com.unimag.travel.mapper.ReservaMapper;
 import com.unimag.travel.repositories.ReservaRepository;
 import com.unimag.travel.services.ReservaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,31 +24,37 @@ public class ReservaServiceImpl implements ReservaService {
 
 
     @Override
-    public Optional<Reserva> getReservaById(Long id) {
-        return reservaRepository.findById(id);
+    public Optional<GetReserva> getReservaById(Long id) {
+        Optional<Reserva> reservaOptional = reservaRepository.findById(id);
+        return reservaOptional.map(ReservaMapper.INSTANCE::reservaToGetReserva);
     }
 
     @Override
-    public List<Reserva> getAllReservas() {
-        return reservaRepository.findAll();
+    public List<GetReserva> getAllReservas() {
+        List<Reserva> reservas = reservaRepository.findAll();
+        return ReservaMapper.INSTANCE.reservaListToGetReservaList(reservas);
     }
 
     @Override
-    public Reserva saveReserva(Reserva reserva) {
-        return reservaRepository.save(reserva);
+    public GetReserva createReserva(SaveReserva saveReserva) {
+        Reserva reserva = ReservaMapper.INSTANCE.saveReservaToReserva(saveReserva);
+        Reserva reservaSaved = reservaRepository.save(reserva);
+
+        return ReservaMapper.INSTANCE.reservaToGetReserva(reservaSaved);
     }
 
     @Override
-    public Reserva updateReservaById(Long id, Reserva saveReserva) {
+    public GetReserva updateReservaById(Long id, SaveReserva saveReserva) {
         Reserva reservaFromDb = reservaRepository.findById(id).get();
         if (reservaFromDb != null) {
-            reservaFromDb.setFechaDeReserva(saveReserva.getFechaDeReserva());
-            reservaFromDb.setCliente(saveReserva.getCliente());
-            reservaFromDb.setNumeroDePasajeros(saveReserva.getNumeroDePasajeros());
+            reservaFromDb.setFechaDeReserva(saveReserva.fechaDeReserva());
+            reservaFromDb.getCliente().setIdCliente(saveReserva.idCliente());
+            reservaFromDb.getVuelo().setIdVuelo(saveReserva.idVuelo());
+            reservaFromDb.setNumeroDePasajeros(saveReserva.numeroDePasajeros());
         } else{
             throw new RuntimeException("reserva not found");
         }
-        return reservaRepository.save(reservaFromDb);
+        return ReservaMapper.INSTANCE.reservaToGetReserva(reservaRepository.save(reservaFromDb));
     }
 
     @Override
