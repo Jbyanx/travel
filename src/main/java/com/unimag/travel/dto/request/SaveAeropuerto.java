6e727b44ad03ae44c568baeac72cc1602d30bb61
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-11-20 22:07:28 -0500

    agregando validaciones de Jakarta validations a los dto y entidades

--- src/main/java/com/unimag/travel/dto/request/SaveAeropuerto.java
+++ src/main/java/com/unimag/travel/dto/request/SaveAeropuerto.java
@@ -7,13 +7,13 @@ import java.io.Serializable;
 
 public record SaveAeropuerto(
         @NotBlank(message = "el nombre del aeropuerto no debe ir vacio")
-        @Size(max = 100)
+        @Size(max = 100, message = "el nombre no debe exceder {max} caracteres")
         String nombre,
         @NotBlank(message = "la ciudad del aeropuerto no debe ir vacia")
-        @Size(max = 100)
+        @Size(max = 100, message = "la ciudad no debe exceder {max} caracteres")
         String ciudad,
         @NotBlank(message = "el pais del aeropuerto no debe ir vacio")
-        @Size(max = 100)
+        @Size(max = 100, message = "el pais no debe exceder {max} caracteres")
         String pais
 ) implements Serializable {
 }
