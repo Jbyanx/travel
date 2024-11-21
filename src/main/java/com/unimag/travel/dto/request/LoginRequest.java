a34989048f631d25042b3576a27f286ca3d4c2ad
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-11-20 22:08:33 -0500

    agregando validaciones de Jakarta validations a los dto y entidades

--- src/main/java/com/unimag/travel/dto/request/LoginRequest.java
+++ src/main/java/com/unimag/travel/dto/request/LoginRequest.java
@@ -1,7 +1,11 @@
 package com.unimag.travel.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequest(
+        @NotBlank(message = "El campo 'correo electrónico' no debe estar vacío")
         String correoElectronico,
+        @NotBlank(message = "El campo 'password' no debe estar vacío")
         String password
 ) {
 }
