1568785929b091abcf024f7ccbb5fae20473ae1e
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-11-20 22:07:04 -0500

    agregando validaciones de Jakarta validations a los dto y entidades

--- src/main/java/com/unimag/travel/entities/Vuelo.java
+++ src/main/java/com/unimag/travel/entities/Vuelo.java
@@ -1,15 +1,16 @@
 package com.unimag.travel.entities;
 
+import com.unimag.travel.entities.Aerolinea;
+import com.unimag.travel.entities.Aeropuerto;
+import com.unimag.travel.entities.Escala;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -18,47 +19,56 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Vuelo {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idVuelo;
+
     @NotBlank(message = "la ciudad origen del vuelo no debe ir vacia")
-    @Size(max = 100)
+    @Size(max = 100, message = "la ciudad origen del vuelo no debe exceder {max} caracteres")
     private String origen;
+
     @NotBlank(message = "la ciudad destino del vuelo no debe ir vacia")
-    @Size(max = 100)
+    @Size(max = 100, message = "la ciudad destino del vuelo no debe exceder {max} caracteres")
     private String destino;
+
     @Column(name = "fecha_de_salida")
     @FutureOrPresent
     private LocalDate fechaDeSalida;
+
     @Column(name = "hora_de_salida")
     @NotNull
     private LocalTime horaDeSalida;
+
     @NotNull
     private Duration duracion;
+
     @Min(value = 1, message = "el vuelo no puede ir sin pasajeros")
+    @Max(value = 500, message = "La capacidad máxima es 500 pasajeros")
     private int capacidad;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "id_aerolinea")
-    @NotNull
+    @NotNull(message = "La aerolínea es obligatoria")
     private Aerolinea aerolinea;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "id_aeropuerto_origen")
-    @NotNull
+    @NotNull(message = "El aeropuerto de origen es obligatorio")
     private Aeropuerto aeropuertoOrigen;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "id_aeropuerto_destino")
-    @NotNull
+    @NotNull(message = "El aeropuerto de destino es obligatorio")
     private Aeropuerto aeropuertoDestino;
-    
+
     @ManyToMany
     @JoinTable(
             name = "vuelos_escalas",
             joinColumns = @JoinColumn(name = "id_vuelo"),
             inverseJoinColumns = @JoinColumn(name = "id_escala")
     )
-    private List<Escala> escalas;
+    private List<Escala> escalas = new ArrayList<>();
 }
