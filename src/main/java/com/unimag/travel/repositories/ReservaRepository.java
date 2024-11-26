73d161a14ec4cb1e534a1eec4780b9a857f7585c
RAAR-18 <raacuna@unimagdalena.edu.co>
2024-11-26 14:31:33 -0500

    buscar Cliente por id en reservas

--- src/main/java/com/unimag/travel/repositories/ReservaRepository.java
+++ src/main/java/com/unimag/travel/repositories/ReservaRepository.java
@@ -4,9 +4,10 @@ import com.unimag.travel.entities.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-
+    List<Reserva> findByCliente_IdCliente(Long IdCliente);
 }
