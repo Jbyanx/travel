a890d1c194fe8b3fbd8c42a3204b8f7cf262f5c6
RAAR-18 <raacuna@unimagdalena.edu.co>
2024-11-25 21:11:37 -0500

    corrigiendo

--- src/main/java/com/unimag/travel/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/unimag/travel/security/services/UserDetailsServiceImpl.java
@@ -1,12 +1,21 @@
 package com.unimag.travel.security.services;
 
 import com.unimag.travel.entities.Cliente;
+import com.unimag.travel.entities.Role;
 import com.unimag.travel.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -17,10 +26,19 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         this.clienteRepository = clienteRepository;
     }
 
+    //Metodo para traer una lista de autoridades por medio de una lista de roles
+    public Collection<GrantedAuthority> mapToAutorities(List<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList());
+    }
+
+    //Metodo para traer un usuario por medio de username (correoElectronico)
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Cliente cliente = clienteRepository.findByCorreoElectronico(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
-        return UserDetailsImpl.build(cliente);
+
+        //Convertir un Set de roles a una lista
+        List<Role> roles = new ArrayList<>(cliente.getRoles());
+        return new User(cliente.getCorreoElectronico(), cliente.getPassword(), mapToAutorities(roles));
     }
 }
