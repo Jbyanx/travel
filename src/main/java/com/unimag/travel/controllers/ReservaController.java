23dd28b6c95db8bf841bd83f011fee4ae50c39b9
RAAR-18 <raacuna@unimagdalena.edu.co>
2024-11-26 14:30:46 -0500

    nuevo metodo para filtrar las reservas por clientes

--- src/main/java/com/unimag/travel/controllers/ReservaController.java
+++ src/main/java/com/unimag/travel/controllers/ReservaController.java
@@ -2,12 +2,16 @@ package com.unimag.travel.controllers;
 
 import com.unimag.travel.dto.request.SaveReserva;
 import com.unimag.travel.dto.response.GetReserva;
+import com.unimag.travel.entities.Cliente;
 import com.unimag.travel.entities.Reserva;
+import com.unimag.travel.repositories.ClienteRepository;
+import com.unimag.travel.security.services.UserDetailsImpl;
 import com.unimag.travel.services.ReservaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -18,17 +22,33 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/reservas")
 public class ReservaController {
+    private final ClienteRepository clienteRepository;
     private ReservaService reservaService;
 
     @Autowired
-    public ReservaController(ReservaService reservaService) {
+    public ReservaController(ReservaService reservaService, ClienteRepository clienteRepository) {
         this.reservaService = reservaService;
+        this.clienteRepository = clienteRepository;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<List<GetReserva>> getAllReservas(){
         List<GetReserva> reservaList = reservaService.getAllReservas();
         return ResponseEntity.ok(reservaList);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/misreservas")
+    public ResponseEntity<List<GetReserva>> getMisreservas(){
+        Long idCliente = getAuthenticatedIdCliente();
+        List<GetReserva> reservaList = reservaService.getReservasByCliente(idCliente);
+        return ResponseEntity.ok(reservaList);
+    }
+
+    private Long getAuthenticatedIdCliente(){
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userDetails.getId();
     }
 
     @GetMapping("/{id}")
